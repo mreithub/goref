fd2f685cedbebf7059fc5f9a71dbd0b3c89481d0
Manuel Reithuber <git@manuel.reithuber.net>
2017-04-22 20:23:24 +0200

    added goref.go

---
+++ goref.go
@@ -0,0 +1,115 @@
+package goref
+
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// TODO tracking execution time here might cause performance issues (e.g. in virtualized environments gettimeofday() might be slow)
+//   if that turns out to be the case, deactivate Data.totalNsec
+
+// singleton GoRef instance
+var instance = NewGoRef()
+
+// Data -- RefCounter data
+type Data struct {
+	refCount   int32
+	totalCount int64
+	totalNsec  int64
+}
+
+// GoRef -- A simple, thread safe key-based reference counter that can be used for profiling your application
+type GoRef struct {
+	data map[string]*Data
+	lock *sync.Mutex
+
+	// linked list to old snapshots
+	lastSnapshot *GoRef
+}
+
+// Instance - Trackable instance
+type Instance struct {
+	parent    *GoRef
+	key       string
+	startTime time.Time
+}
+
+// Deref -- Dereference an instance of 'key'
+func (i Instance) Deref() {
+	now := time.Now()
+	data := i.parent.get(i.key)
+	atomic.AddInt32(&data.refCount, -1)
+	nsec := now.Sub(i.startTime).Nanoseconds()
+	log.Print("nsec: ", nsec)
+	atomic.AddInt64(&data.totalNsec, nsec)
+}
+
+// get -- Get the Data object for the specified key (or create it)
+func (g *GoRef) get(key string) *Data {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+
+	rc, ok := g.data[key]
+	if !ok {
+		rc = &Data{}
+		g.data[key] = rc
+	}
+
+	return rc
+}
+
+// Clone -- Returns a copy of the GoRef  (synchronously)
+func (g *GoRef) Clone() GoRef {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+
+	data := map[string]*Data{}
+
+	for key, d := range g.data {
+		data[key] = &Data{
+			refCount:   d.refCount,
+			totalCount: d.totalCount,
+			totalNsec:  d.totalNsec,
+		}
+	}
+
+	// return a cloned GoRef instance
+	return GoRef{
+		data:         data,
+		lock:         nil, // clones are (meant to be) read-only -> no need for locks
+		lastSnapshot: nil, //
+	}
+}
+
+// Ref -- References an instance of 'key'
+func (g *GoRef) Ref(key string) Instance {
+	data := g.get(key)
+	atomic.AddInt32(&data.refCount, 1)
+	atomic.AddInt64(&data.totalCount, 1)
+
+	return Instance{
+		parent:    g,
+		key:       key,
+		startTime: time.Now(),
+	}
+}
+
+// TakeSnapshot -- Clone the current GoRef instance and return
+func (g *GoRef) TakeSnapshot() GoRef {
+	old := g.lastSnapshot
+	rc := g.Clone()
+	rc.lastSnapshot = old
+	g.lastSnapshot = &rc
+	return rc
+}
+
+// NewGoRef -- GoRef constructor
+func NewGoRef() *GoRef {
+	return &GoRef{
+		lock:         &sync.Mutex{},
+		data:         map[string]*Data{},
+		lastSnapshot: nil,
+	}
+}
