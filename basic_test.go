99ff2627ad67b2d4ec005d74aae26c912a4067f0
Manuel Reithuber <git@manuel.reithuber.net>
2017-04-22 21:08:56 +0200

    basic_test.go: added panic tests (to make sure you can't call Get(), GetData() and Keys() on live instances)

--- basic_test.go
+++ basic_test.go
@@ -20,6 +20,9 @@ func TestBasics(t *testing.T) {
 	clone3 := g.Clone()
 	ref.Deref()
 
+	// all the assertions are done after the fact (to make sure the different clones
+	// keep their own copies of the Data)
+
 	// final (current) state
 	assert.Contains(t, g.data, "hello")
 	assert.Contains(t, g.data, "world")
@@ -40,6 +43,7 @@ func TestBasics(t *testing.T) {
 	assert.Equal(t, int32(0), d1.RefCount)
 	assert.Equal(t, int64(1), d1.TotalCount)
 	assert.True(t, d1.TotalNsec > 0)
+	assert.Equal(t, 1, len(clone1.GetData()))
 
 	// clone2: clone1 + Ref('world'),  sleep(100ms)
 	keys = clone2.Keys()
@@ -60,4 +64,31 @@ func TestBasics(t *testing.T) {
 	assert.True(t, d3.TotalNsec >= 100000000)
 	assert.True(t, clone3.Get("hello").TotalNsec < 100000)
 	assert.NotEqual(t, d1.TotalNsec, d3.TotalNsec)
+}
+
+func TestGetPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			assert.Fail(t, "Expected a panic() in GoRef.Get()")
+		}
+	}()
+	NewGoRef().Get("foo")
+}
+
+func TestGetDataPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			assert.Fail(t, "Expected a panic() in GoRef.GetData()")
+		}
+	}()
+	NewGoRef().GetData()
+}
+
+func TestKeysPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			assert.Fail(t, "Expected a panic() in GoRef.Keys()")
+		}
+	}()
+	NewGoRef().Keys()
 }
