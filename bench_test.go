950fbaa8f362c8c22fb4bdfc849459fa0db81aef
Manuel Reithuber <git@manuel.reithuber.net>
2017-06-02 18:22:45 +0200

    added Clone() benchmark (one for 100 elements, one for 1000)

--- bench_test.go
+++ bench_test.go
@@ -1,11 +1,13 @@
 package goref
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 var nsec int64
+var snap Snapshot
 
 // BenchmarkMeasureTime -- Measures how long measuring the time takes (using time.Now() and Time.Sub())
 func BenchmarkMeasureTime(b *testing.B) {
@@ -40,4 +42,25 @@ func BenchmarkRefDerefDeferred(b *testing.B) {
 	//snap := g.Clone()
 	//j, _ := json.Marshal(snap.Data)
 	//log.Printf("data: %s", j)
+}
+
+// benchmarkGetSnapshot -- Measure how long it takes to create a deep copy of the snapshot data
+func benchmarkGetSnapshot(count int, b *testing.B) {
+	// setup
+	g := NewGoRef()
+	for n := 0; n < count; n++ {
+		g.Ref(fmt.Sprintf("ref%d", n)).Deref()
+	}
+
+	for n := 0; n < b.N; n++ {
+		snap = *g.Clone()
+	}
+}
+
+func BenchmarkGetSnapshot100(b *testing.B) {
+	benchmarkGetSnapshot(100, b)
+}
+
+func BenchmarkGetSnapshot1000(b *testing.B) {
+	benchmarkGetSnapshot(1000, b)
 }
